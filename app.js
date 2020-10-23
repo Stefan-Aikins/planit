1ce38f4305f8d18230abc76d76c11192f4a509df
Stefan Aikins <stefanaikins@gmail.com>
2020-10-23 14:45:54 +0100

    dayte array added

---
+++ app.js
@@ -0,0 +1,81 @@
+//created this array to loop through
+let thisDay = [
+
+    {
+        unique : '0',
+        hourOfDay : '09',
+        time : '09',
+        GTM : 'am',
+        notification : ''
+    },
+
+    {
+        unique : '1',
+        hourOfDay : '10',
+        time : '10',
+        GTM : 'am',
+        notification : ''
+    },
+
+    {
+        unique : '2',
+        hourOfDay : '11',
+        time : '11',
+        GTM : 'am',
+        notification : ''
+    },
+    //new day starts
+    {
+        unique : '3',
+        hourOfDay : '12',
+        time : '12',
+        GTM : 'pm',
+        notification : ''
+    },
+
+    {
+        unique : '4',
+        hourOfDay : '01',
+        time : '13',
+        GTM : 'pm',
+        notification : ''
+    },
+
+    {
+        unique : '5',
+        hourOfDay : '02',
+        time : '14',
+        GTM : 'pm',
+        notification : ''
+    },
+
+    {
+        unique : '6',
+        hourOfDay : '03',
+        time : '15',
+        GTM : 'pm',
+        notification : ''
+    },
+
+    {
+        unique : '7',
+        hourOfDay : '04',
+        time : '16',
+        GTM : 'pm',
+        notification : ''
+    },
+
+    {
+        unique : '8',
+        hourOfDay : '05',
+        time : '17',
+        GTM : 'am',
+        notification : ''
+    }
+
+];
+console.log(thisDay)
+/*function plannerDate () {
+    var dateNow = moment ().format ()
+}*/
+
